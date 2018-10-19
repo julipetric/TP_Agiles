20a316d3bc2aac0f6243549df9485167f77d679e
matijacob96 <matijacob@live.com>
2018-10-19 15:20:59 -0300

    Los Daos fueron desarolladazos

--- Project_Agiles/src/main/java/Daos/DomicilioDao.java
+++ Project_Agiles/src/main/java/Daos/DomicilioDao.java
@@ -5,10 +5,95 @@
  */
 package Daos;
 
+import Modelo.Domicilio;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author matij
  */
 public class DomicilioDao {
-    
+
+    private static Session sesion;
+
+    public static Session getSesion() {
+        return sesion;
+    }
+
+    public static void setSesion(Session sesion) {
+        DomicilioDao.sesion = sesion;
+    }
+
+    public static Domicilio find(Integer uid) {
+        Transaction tx = null;
+        List domicilios = new ArrayList<>();
+        try {
+            tx = sesion.beginTransaction();
+            domicilios = sesion.createQuery("FROM Domicilio d WHERE d.uid ='" + uid).list();
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        if (domicilios.size() == 1) {
+            return (Domicilio) domicilios.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    public static ArrayList<Domicilio> getAllDomicilios() {
+        Transaction tx = null;
+        List domicilios = new ArrayList<>();
+        try {
+            tx = sesion.beginTransaction();
+            domicilios = sesion.createQuery("FROM Usuario").list();
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        ArrayList<Domicilio> dom = new ArrayList<>();
+        for (Object o : domicilios) {
+            dom.add((Domicilio) o);
+        }
+        return dom;
+    }
+
+    public static void modify(Domicilio mD) {
+        Transaction tx = null;
+        try {
+            tx = sesion.beginTransaction();
+            sesion.update(mD);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public static void insert(Domicilio nD) {
+        Transaction tx = null;
+        try {
+            tx = sesion.beginTransaction();
+            sesion.save(nD);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 }
--- Project_Agiles/src/main/java/Daos/LicenciaDao.java
+++ Project_Agiles/src/main/java/Daos/LicenciaDao.java
@@ -5,10 +5,110 @@
  */
 package Daos;
 
+import Modelo.Licencia;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author matij
  */
 public class LicenciaDao {
+        private static Session sesion;
+
+    public static Session getSesion() {
+        return sesion;
+    }
+
+    public static void setSesion(Session sesion) {
+        LicenciaDao.sesion = sesion;
+    }
+
+    public static Licencia find(Integer uid) {
+        Transaction tx = null;
+        List licencias = new ArrayList<>();
+        try {
+            tx = sesion.beginTransaction();
+            licencias = sesion.createQuery("FROM Licencia l WHERE l.uid ='" + uid).list();
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        if (licencias.size() == 1) {
+            return (Licencia) licencias.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    public static ArrayList<Licencia> getAllLicencia() {
+        Transaction tx = null;
+        List licencias = new ArrayList<>();
+        try {
+            tx = sesion.beginTransaction();
+            licencias = sesion.createQuery("FROM Usuario").list();
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        ArrayList<Licencia> lic = new ArrayList<>();
+        for (Object o : licencias) {
+            lic.add((Licencia) o);
+        }
+        return lic;
+    }
+
+    public static void modify(Licencia mL) {
+        Transaction tx = null;
+        try {
+            tx = sesion.beginTransaction();
+            sesion.update(mL);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public static void insert(Licencia nL) {
+        Transaction tx = null;
+        try {
+            tx = sesion.beginTransaction();
+            sesion.save(nL);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
     
+    public static ArrayList<Licencia> getExpiradas(){
+        Transaction tx = null;
+        List licenciasexpiradas = new ArrayList<>();
+        try{
+            tx = sesion.beginTransaction();
+            licenciasexpiradas = sesion.createQuery("FROM Licencia l WHERE l.fechaExpiracion <= CURDATE()").list();
+        } catch (HibernateException e) {
+            
+        }
+        ArrayList<Licencia> lic = new ArrayList<>();
+        for (Object o : licenciasexpiradas) {
+            lic.add((Licencia) o);
+        }
+        return lic;
+    }
 }
